e8976f2720f05bb9f9ca3b3623dcd25c19396597
raghvendra221 <raghvendrayadav8171@gmail.com>
2025-11-06 03:33:19 +0530

    style fix

--- jobportal/settings.py
+++ jobportal/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = config('SECRET_KEY', default='unsafe-secret-key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = True
-DEBUG = config('DEBUG', default=True, cast=bool)
+DEBUG = config('DEBUG', default=False, cast=bool)
 
 ALLOWED_HOSTS = ['*']
 
